d57990d27342e937a491979197b5ca3edc75be4b
xiaocainiao <liangnan1993@163.com>
2013-10-20 12:13:47 +0800

    2013.10.20 xiaocainiao upload test
    just modify the index page

---
+++ upload/data/log/201310_illegallog.php
@@ -0,0 +1,1 @@
+<?PHP exit;?>	1382167608	liangnani	***	Ques #0	127.0.0.1
---
+++ upload/data/log/201310_sendmail.php
@@ -0,0 +1,2 @@
+<?PHP exit;?>	2013-10-19 15:09:43	127.0.0.1	1	/upload/home.php?mod=spacecp&ac=profile	liangnan1993@163.com sendmail failed.
+
