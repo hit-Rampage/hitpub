8f76c5b4200c021d12d30ba6a238e617c783d167
xiaocainiao <liangnan1993@163.com>
2013-11-17 17:29:50 +0800

    merge the changes of mine and ma's
    menhu&luntan&gerenzhuye

---
+++ upload/data/log/201311_illegallog.php
@@ -0,0 +1,1 @@
+<?PHP exit;?>	1384665330	liangnani	***	Ques #0	127.0.0.1
---
+++ upload/data/log/201311_modcp.php
@@ -0,0 +1,4 @@
+<?PHP exit;?>	1384651796	admin	1	127.0.0.1	login		42	GET={mod=modcp; fid=42; action=login; }; POST={};
+<?PHP exit;?>	1384651803	admin	1	127.0.0.1	thread		42	GET={mod=modcp; fid=42; action=thread; }; POST={};
+<?PHP exit;?>	1384651818	admin	1	127.0.0.1	thread	thread	42	GET={mod=modcp; action=thread; fid=42; op=thread; do=list; }; POST={};
+<?PHP exit;?>	1384651824	admin	1	127.0.0.1	thread	thread	42	GET={mod=modcp; action=thread; op=thread; fid=42; }; POST={};
